a728380ef41f00d2593a07600882fcf3fcd94fef
Mbzneto <72531253+Mbzneto@users.noreply.github.com>
2025-04-24 20:13:13 -0300

    eita

--- dataManager.gd
+++ dataManager.gd
@@ -12,8 +12,13 @@ func _ready() -> void:
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 	jsonData = {"1":{"a":0, "b":0}, "2":{"a":0, "b":0}}
 	dataStart.emit(sData.stationData)
+=======
+	userData = StationData.new()
+	dataStart.emit(userData.stationData)
+>>>>>>> Stashed changes
 =======
 	userData = StationData.new()
 	dataStart.emit(userData.stationData)
