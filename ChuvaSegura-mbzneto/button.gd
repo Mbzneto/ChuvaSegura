1cd76881dd1549b6e10ad901faac96db482da1c3
caspariga <robson.matheus.rv@gmail.com>
2025-04-24 22:45:50 -0300

    Add files via upload
    aumentei a margem do texto pros botões das linhas/estações

---
+++ ChuvaSegura-mbzneto/button.gd
@@ -0,0 +1,19 @@
+extends Button
+
+signal pressedLinha(data)
+
+var data = []
+var OptionButtona
+var OptionButton2
+var DataManager
+
+func _ready() -> void:
+	OptionButtona = get_node("../OptionButton")
+	OptionButton2 = get_node("../OptionButton2")
+
+
+func _on_pressed() -> void:
+	if OptionButtona.item_count > 0 and OptionButton2.item_count > 0:
+		data = [OptionButtona.get_item_text(OptionButtona.selected), OptionButton2.get_item_text(OptionButton2.selected)]
+		if data[0] != "" and data[1] != "":
+			pressedLinha.emit(data)
